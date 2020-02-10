66ca4863d333dd4e1968f1629ef003e6a25d955f
Daniel Zimmermann <daniel.zimmermann.dz@gmail.com>
2020-02-10 21:02:12 +0100

    Better movement and fullscreen support

--- settings.py
+++ settings.py
@@ -1,10 +1,16 @@
 """A module for the settings of the Alien Invasion game"""
+
+
 class Settings:
     """A class to store all settings for the game"""
 
     def __init__(self):
         """Initialize the game's settings"""
         # Screen settings
+        self.fullscreen = True
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
